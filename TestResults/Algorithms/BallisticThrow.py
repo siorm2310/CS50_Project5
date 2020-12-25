575be28d3380f183073229bcfd0503e43205545e
Sior Meir <siorm2310@gmail.com>
2020-12-25 19:46:17 +0200

    Projectile formulas implemented

--- TestResults/Algorithms/BallisticThrow.py
+++ TestResults/Algorithms/BallisticThrow.py
@@ -1,23 +1,33 @@
 import math
 import numpy as np
+
+g = 9.81
+
 class VectorProperty():
-    """
-    docstring
-    """
     def __init__(self,x_property,y_property):
         self.x = x_property
-        self.y = y_property
+        if y_property < 0:
+            self.y = 0
+        else:
+            self.y = y_property
         self.length = math.sqrt((self.x)**2 + (self.y)**2)
         self.arg = math.tan(self.y/self.x)
 
+class InitialConditions():
+    def __init__(self,x_position,y_position,x_velocity,y_velocity):
+        self.location = VectorProperty(x_position,y_position)
+        self.velocity = VectorProperty(x_velocity,y_velocity)
 
-class InitialConditions(VectorProperty):
-    pass
+    def calc_landing(self):
+        v = self.velocity.length
+        theta = self.velocity.arg
+        h = self.location.x
+        t_tof = (1/g) * (v * math.sin(theta) + math.sqrt(v * (math.sin(theta))**2) + 2 * g * h) # Time of flight
+        range_flight = self.location.x + self.velocity.x * t_tof
+        h_max = h + v**2 * (math.sin(theta))**2 / (2 * g)
+        return {
+            "Time of Flight [s]" : t_tof,
+            "Range of Flight [m]" : range_flight,
+            "Maximum Height [m]" : h_max
+        }
 
-
-
-def calc_landing_properies(initial_conditions):
-    """
-    docstring
-    """
-    t_landing = 0
