15a23b9c0012cd979829f968d3635500a41c4467
Sior Meir <siorm2310@gmail.com>
2021-01-08 16:26:59 +0200

    tal

--- TestResults/Algorithms/BallisticThrow.py
+++ TestResults/Algorithms/BallisticThrow.py
@@ -1,42 +1,44 @@
 import math
 import numpy as np
 
-g = 9.81
 
 class VectorProperty():
-    def __init__(self,x_property,y_property,length=None,arg=None):
-        if length is None:
-            self.x = x_property
-            self.y = y_property
-            self.length = math.sqrt((self.x)**2 + (self.y)**2)
+    def __init__(self,first_property,second_property,is_polar=False):
+        if not is_polar:
+            self.x = first_property
+            self.y = second_property
+            self.magnitude = math.sqrt((self.x)**2 + (self.y)**2)
             self.arg = math.tan(self.y/self.x)
         else:
-            self.length = length
-            self.arg = arg
-            self.x = length * math.cos(arg)
-            self.y = length * math.sin(arg)
+            self.magnitude = first_property
+            self.arg = second_property
+            self.x = self.magnitude * math.cos(self.arg)
+            self.y = self.magnitude * math.sin(self.arg)
 
     def validate_location(self):
-        if self.y >= 0 and self.length > 1e-5:
+        if self.y >= 0 and self.magnitude > 1e-5:
             return True
         return False
 
-class InitialConditions():
-    def __init__(self,x_position,y_position,x_velocity,y_velocity):
+class BallisticThrow():
+
+    g = 9.81
+
+    def __init__(self,x_position,y_position,velocity,angle):
         self.location = VectorProperty(x_position,y_position)
-        self.velocity = VectorProperty(x_velocity,y_velocity)
+        self.velocity = VectorProperty(velocity,angle,is_polar=True)
 
     def calc_landing(self):
-        v = self.velocity.length
+        v = self.velocity.magnitude
         theta = self.velocity.arg
         h = self.location.x
-        t_tof = (1/g) * (v * math.sin(theta) + math.sqrt(v * (math.sin(theta))**2) + 2 * g * h) # Time of flight
+        t_tof = (1/self.g) * (v * math.sin(theta) + math.sqrt(v * (math.sin(theta))**2) + 2 * self.g * h) # Time of flight
         range_flight = self.location.x + self.velocity.x * t_tof
         h_max = h + v**2 * (math.sin(theta))**2 / (2 * g)
         return {
-            "Time of Flight [s]" : t_tof,
-            "Range of Flight [m]" : range_flight,
-            "Maximum Height [m]" : h_max
+            "Time" : t_tof,
+            "MaxRange" : range_flight,
+            "MaxHeight" : h_max
         }
 
     def create_datapoints(self):
